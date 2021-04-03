97848654461f569fb97a302e2661bacd1baa4e0c
Igor Segalla <igorsegallafa@gmail.com>
2021-04-03 00:57:13 -0300

    Spotlight layout

--- lib/spotlight_web/live/spotlight_live.ex
+++ lib/spotlight_web/live/spotlight_live.ex
@@ -1,11 +1,30 @@
 defmodule SpotlightWeb.SpotlightLive do
   use SpotlightWeb, :live_view
 
+  @results %{
+    "A" => [
+      "Amazon",
+      "American"
+    ],
+  }
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, results: @results)}
   end
 
   def render(assigns) do
     render(SpotlightWeb.SpotlightView, "index.html", assigns)
+  end
+
+  def handle_event("search", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("select", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("next_result", params, socket) do
+    {:noreply, socket}
   end
 end
